5ed574105bbb81309f46264de7a305aee9a60fb6
Sanskar Singhal <Sanskar.Singhal@cginfinity.com>
2026-07-27 16:13:01 +0530

    trigger build

--- BillingDB-Frontend/src/api/ApiClient.jsx
+++ BillingDB-Frontend/src/api/ApiClient.jsx
@@ -4,4 +4,6 @@ const apiClient = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL
 });
 
+console.log("backend url is", import.meta.env.VITE_API_BASE_URL);
+
 export default apiClient;
